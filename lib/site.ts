77015824cf8b5fb9ef73cc0e5c491c750636b2f1
jjsometimes51 <jjsometimes51@gmail.com>
2026-05-20 13:35:30 +0200

    Add files via upload

---
+++ lib/site.ts
@@ -0,0 +1,6 @@
+export const site = {
+  name: 'Ground & Light',
+  url: 'https://www.groundandlight.space',
+  description: 'A record of places, work, thought, and attention.',
+  slogan: 'Abide nowhere. Let the mind flow freely.'
+}
